070454d544860baed2949940ba28c8c8d41e7062
Walter <walter@Mac-mini-de-Walter.local>
2026-02-11 14:31:37 -0500

    actualizacion

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/browser'));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/browser/index.html'));
+});
+
+// Use PORT from environment or default to 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor Frontend corriendo en puerto ${PORT}`);
+});
