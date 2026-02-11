b4a548c8186ab9fc521e4dc6e820f3eecb1807b7
Walter <walter@Mac-mini-de-Walter.local>
2026-02-11 16:29:06 -0500

    feat: update api url to custom domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://walter-elfarodelsigloapi-x8zfr0-af2ec4-34-55-40-237.traefik.me'
+    apiUrl: 'https://elfarodelsiglo_api.walther1991.digital'
 };
